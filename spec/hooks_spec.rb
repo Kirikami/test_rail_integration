a0d6fa4f26b9e776b2abc068e36108a6367d0c66
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2015-03-23 10:31:39 +0700

    Refactored hooks class tests to use context

--- spec/hooks_spec.rb
+++ spec/hooks_spec.rb
@@ -12,107 +12,123 @@ describe 'Update test results' do
     allow(TestRail::Connection).to receive(:change_test_run_name).and_return('changed!')
   end
 
-  it 'test that test rail data is correct if there is no previous result and current is passed ( -> P )' do
-    allow(TestRail::Connection).to receive(:get_test_result).and_return([])
-    allow(@scenario).to receive(:passed?).and_return(true)
-    test_result = TestRail::Hook.update_test_rail(@scenario)
-    expect(test_result.test_case_id).to eq('4556')
-    expect(test_result.title).to eq('title')
-    expect(test_result.comment).to eq({:status=>1, :comment => 'test **passed:**'})
-    expect(test_result.to_test_rail_api).to eq({:status_id => 1, :comment => "test **passed:** \"title\""})
+  context 'test that test rail data is correct if there is no previous result' do
+
+    before(:each) do
+      allow(TestRail::Connection).to receive(:get_test_result).and_return([])
+    end
+
+    it 'current result should be passed' do
+      allow(@scenario).to receive(:passed?).and_return(true)
+      test_result = TestRail::Hook.update_test_rail(@scenario)
+      expect(test_result.test_case_id).to eq('4556')
+      expect(test_result.title).to eq('title')
+      expect(test_result.comment).to eq({:status=>1, :comment => 'test **passed:**'})
+      expect(test_result.to_test_rail_api).to eq({:status_id => 1, :comment => "test **passed:** \"title\""})
+    end
+
+    it 'current result should be failed' do
+      allow(@scenario).to receive(:passed?).and_return(false)
+      steps = double('steps')
+      allow(steps).to receive(:exception).and_return('exception')
+      allow(@scenario).to receive(:steps).and_return(steps)
+      test_result = TestRail::Hook.update_test_rail(@scenario)
+      expect(test_result.test_case_id).to eq('4556')
+      expect(test_result.title).to eq('title')
+      expect(test_result.comment).to eq({:status=>5, :comment => 'test **failed:**'})
+      expect(test_result.to_test_rail_api).to eq({:status_id => 5, :comment => "test **failed:** \"title\"\n Exception : exception\n "})
+    end
+
   end
 
-  it 'test that test rail data is correct if there is no previous result and current is failed ( -> F )' do
-    allow(TestRail::Connection).to receive(:get_test_result).and_return([])
-    allow(@scenario).to receive(:passed?).and_return(false)
-    steps = double('steps')
-    allow(steps).to receive(:exception).and_return('exception')
-    allow(@scenario).to receive(:steps).and_return(steps)
-    test_result = TestRail::Hook.update_test_rail(@scenario)
-    expect(test_result.test_case_id).to eq('4556')
-    expect(test_result.title).to eq('title')
-    expect(test_result.comment).to eq({:status=>5, :comment => 'test **failed:**'})
-    expect(test_result.to_test_rail_api).to eq({:status_id => 5, :comment => "test **failed:** \"title\"\n Exception : exception\n "})
+  context 'test that test rail data is correct if previous result is passed' do
+
+    before(:each) do
+      allow(TestRail::Connection).to receive(:get_test_result).and_return(
+                                         [{"status_id" => 1, "comment" =>"test **passed:**"}])
+    end
+
+    it 'current result should be passed' do
+      allow(@scenario).to receive(:passed?).and_return(true)
+      test_result = TestRail::Hook.update_test_rail(@scenario)
+      expect(test_result.test_case_id).to eq('4556')
+      expect(test_result.title).to eq('title')
+      expect(test_result.comment).to eq({:status=>1, :comment => 'test **passed:**'})
+      expect(test_result.to_test_rail_api).to eq({:status_id => 1, :comment => "test **passed:** \"title\""})
+    end
+
+    it 'current result should be failed' do
+      allow(@scenario).to receive(:passed?).and_return(false)
+      steps = double('steps')
+      allow(steps).to receive(:exception).and_return('exception')
+      allow(@scenario).to receive(:steps).and_return(steps)
+      test_result = TestRail::Hook.update_test_rail(@scenario)
+      expect(test_result.test_case_id).to eq('4556')
+      expect(test_result.title).to eq('title')
+      expect(test_result.comment).to eq({:status=>5, :comment => 'test **failed:**'})
+      expect(test_result.to_test_rail_api).to eq({:status_id => 5, :comment => "test **failed:** \"title\"\n Exception : exception\n "})
+    end
+
   end
 
-  it 'test that test rail data is correct if previous result is passed and current is passed (P -> P)' do
-    allow(TestRail::Connection).to receive(:get_test_result).and_return(
-                                       [{"status_id" => 1, "comment" =>"test **passed:**"}])
-    allow(@scenario).to receive(:passed?).and_return(true)
-    test_result = TestRail::Hook.update_test_rail(@scenario)
-    expect(test_result.test_case_id).to eq('4556')
-    expect(test_result.title).to eq('title')
-    expect(test_result.comment).to eq({:status=>1, :comment => 'test **passed:**'})
-    expect(test_result.to_test_rail_api).to eq({:status_id => 1, :comment => "test **passed:** \"title\""})
+  context 'test that test rail data is correct if previous result is failed' do
+
+    before(:each) do
+      allow(TestRail::Connection).to receive(:get_test_result).and_return(
+                                         [{"status_id" => 5, "comment" =>"test **failed:**"}])
+    end
+
+    it 'current result should be failed' do
+      allow(@scenario).to receive(:passed?).and_return(false)
+      steps = double('steps')
+      allow(steps).to receive(:exception).and_return('exception')
+      allow(@scenario).to receive(:steps).and_return(steps)
+      test_result = TestRail::Hook.update_test_rail(@scenario)
+      expect(test_result.test_case_id).to eq('4556')
+      expect(test_result.title).to eq('title')
+      expect(test_result.comment).to eq({:status=>5, :comment => 'test **failed:**'})
+      expect(test_result.to_test_rail_api).to eq({:status_id => 5, :comment => "test **failed:** \"title\"\n Exception : exception\n test **failed:**"})
+    end
+
+    it 'current result should be comment' do
+      allow(@scenario).to receive(:passed?).and_return(true)
+      test_result = TestRail::Hook.update_test_rail(@scenario)
+      expect(test_result.test_case_id).to eq('4556')
+      expect(test_result.title).to eq('title')
+      expect(test_result.comment).to eq({:status=>0, :comment => 'test **passed:**'})
+      expect(test_result.to_test_rail_api).to eq({:comment => "test **passed:** \"title\"\n test **failed:**"})
+    end
+
   end
 
-  it 'test that test rail data is correct if previous result is passed and current is failed (P -> F)' do
-    allow(TestRail::Connection).to receive(:get_test_result).and_return(
-                                       [{"status_id" => 1, "comment" =>"test **passed:**"}])
-    allow(@scenario).to receive(:passed?).and_return(false)
-    steps = double('steps')
-    allow(steps).to receive(:exception).and_return('exception')
-    allow(@scenario).to receive(:steps).and_return(steps)
-    test_result = TestRail::Hook.update_test_rail(@scenario)
-    expect(test_result.test_case_id).to eq('4556')
-    expect(test_result.title).to eq('title')
-    expect(test_result.comment).to eq({:status=>5, :comment => 'test **failed:**'})
-    expect(test_result.to_test_rail_api).to eq({:status_id => 5, :comment => "test **failed:** \"title\"\n Exception : exception\n "})
+  context 'test that test rail data is correct if previous result is comment' do
+
+    before(:each) do
+      allow(TestRail::Connection).to receive(:get_test_result).and_return(
+                                         [{"status_id" => 5, "comment" =>"test **failed:**"}])
+    end
+
+    it 'current result should be comment' do
+      allow(@scenario).to receive(:passed?).and_return(true)
+      test_result = TestRail::Hook.update_test_rail(@scenario)
+      expect(test_result.test_case_id).to eq('4556')
+      expect(test_result.title).to eq('title')
+      expect(test_result.comment).to eq({:status=>0, :comment => 'test **passed:**'})
+      expect(test_result.to_test_rail_api).to eq({:comment => "test **passed:** \"title\"\n test **failed:**"})
+    end
+
+    it 'current result should be failed' do
+      allow(@scenario).to receive(:passed?).and_return(false)
+      steps = double('steps')
+      allow(steps).to receive(:exception).and_return('exception')
+      allow(@scenario).to receive(:steps).and_return(steps)
+      test_result = TestRail::Hook.update_test_rail(@scenario)
+      expect(test_result.test_case_id).to eq('4556')
+      expect(test_result.title).to eq('title')
+      expect(test_result.comment).to eq({:status=>5, :comment => 'test **failed:**'})
+      expect(test_result.to_test_rail_api).to eq({:status_id => 5, :comment => "test **failed:** \"title\"\n Exception : exception\n test **failed:**"})
+    end
+
   end
 
-  it 'test that test rail data is correct if previous result is failed and current is failed (F -> F)' do
-    allow(TestRail::Connection).to receive(:get_test_result).and_return(
-                                       [{"status_id" => 5, "comment" =>"test **failed:**"}])
-    allow(@scenario).to receive(:passed?).and_return(false)
-    steps = double('steps')
-    allow(steps).to receive(:exception).and_return('exception')
-    allow(@scenario).to receive(:steps).and_return(steps)
-    test_result = TestRail::Hook.update_test_rail(@scenario)
-    expect(test_result.test_case_id).to eq('4556')
-    expect(test_result.title).to eq('title')
-    expect(test_result.comment).to eq({:status=>5, :comment => 'test **failed:**'})
-    expect(test_result.to_test_rail_api).to eq({:status_id => 5, :comment => "test **failed:** \"title\"\n Exception : exception\n test **failed:**"})
-  end
-
-  it 'test that test rail data is correct if previous result is failed and current is passed (F -> P)' do
-    allow(TestRail::Connection).to receive(:get_test_result).and_return(
-                                       [{"status_id" => 5, "comment" =>"test **failed:**"}])
-    allow(@scenario).to receive(:passed?).and_return(true)
-    test_result = TestRail::Hook.update_test_rail(@scenario)
-    expect(test_result.test_case_id).to eq('4556')
-    expect(test_result.title).to eq('title')
-    expect(test_result.comment).to eq({:status=>0, :comment => 'test **passed:**'})
-    expect(test_result.to_test_rail_api).to eq({:comment => "test **passed:** \"title\"\n test **failed:**"})
-  end
-
-  it 'test that test rail data is correct if previous result is comment and current is passed (C -> P)' do
-    allow(TestRail::Connection).to receive(:get_test_result).and_return(
-                                       [{"status_id" => 5, "comment" =>"test **failed:**"}])
-    # there must be failed previous results for comment
-    allow(@scenario).to receive(:passed?).and_return(true)
-    test_result = TestRail::Hook.update_test_rail(@scenario)
-    expect(test_result.test_case_id).to eq('4556')
-    expect(test_result.title).to eq('title')
-    expect(test_result.comment).to eq({:status=>0, :comment => 'test **passed:**'})
-    expect(test_result.to_test_rail_api).to eq({:comment => "test **passed:** \"title\"\n test **failed:**"})
-  end
-
-  it 'test that test rail data is correct if previous result is comment and current is failed (C -> F)' do
-    allow(TestRail::Connection).to receive(:get_test_result).and_return(
-                                       [{"status_id" => 5, "comment" =>"test **failed:**"}])
-    # there must be failed previous results for comment
-    allow(@scenario).to receive(:passed?).and_return(false)
-    steps = double('steps')
-    allow(steps).to receive(:exception).and_return('exception')
-    allow(@scenario).to receive(:steps).and_return(steps)
-    test_result = TestRail::Hook.update_test_rail(@scenario)
-    expect(test_result.test_case_id).to eq('4556')
-    expect(test_result.title).to eq('title')
-    expect(test_result.comment).to eq({:status=>5, :comment => 'test **failed:**'})
-    expect(test_result.to_test_rail_api).to eq({:status_id => 5, :comment => "test **failed:** \"title\"\n Exception : exception\n test **failed:**"})
-  end
-
-  after(:each) do
-    allow(TestRail::Connection).to receive(:get_test_result).and_call_original
-  end
 end
